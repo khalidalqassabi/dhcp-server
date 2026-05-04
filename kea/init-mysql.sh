2090e81537cc838cc8da565bc87e0858df790f5f
khalidalqassabi <khq94k@gmail.com>
2026-05-04 09:33:35 +0400

    fix(infra): secure init-mysql.sh password handling; harden nginx TLS

--- kea/init-mysql.sh
+++ kea/init-mysql.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 set -e
 
+read -rsp "Enter MySQL kea user password: " KEA_PASS
+echo
+
 echo "==> Creating MySQL database and user..."
-sudo mysql <<'SQL'
+sudo mysql <<SQL
 CREATE DATABASE IF NOT EXISTS kea;
-CREATE USER IF NOT EXISTS 'kea'@'127.0.0.1' IDENTIFIED BY 'REPLACE_WITH_STRONG_PASSWORD';
+CREATE USER IF NOT EXISTS 'kea'@'127.0.0.1' IDENTIFIED BY '${KEA_PASS}';
 GRANT ALL PRIVILEGES ON kea.* TO 'kea'@'127.0.0.1';
 FLUSH PRIVILEGES;
 SQL
 
 echo "==> Initialising Kea schema..."
-kea-admin db-init mysql -u kea -p REPLACE_WITH_STRONG_PASSWORD -n kea -h 127.0.0.1
+kea-admin db-init mysql -u kea -p "${KEA_PASS}" -n kea -h 127.0.0.1
 
 echo "==> Done. Kea MySQL database is ready."
+echo "==> Remember: use this same password in /etc/kea/kea-dhcp4.conf (3 occurrences)."
